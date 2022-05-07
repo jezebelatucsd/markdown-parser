a7eaa751a725bddf9c7bcc002c2c4437734679b3
jezebelatucsd <103277481+jezebelatucsd@users.noreply.github.com>
2022-05-06 20:11:42 -0700

    adding a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,8 +17,8 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             
-            //break statements
-            
+            //break statements 
+
             if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1) {
                 break;
             }
